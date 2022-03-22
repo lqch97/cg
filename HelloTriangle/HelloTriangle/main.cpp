930abbcf19f308c5718d89c3a48ae343532eb9a6
LQCH <lqch@datalab.cs.nthu.edu.tw>
2022-03-22 15:41:49 +0800

    new project: Hello Triangle

---
+++ HelloTriangle/HelloTriangle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloTriangle
+//
+//  Created by 陳隆琦 on 2022/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
